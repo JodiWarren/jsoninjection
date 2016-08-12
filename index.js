78dfb5e951cf8bf884c0553a507e01f502295fd7
Jodi Warren <jodi@jodiwarren.com>
2016-08-13 00:15:01 +0100

    Add minifier, add gitignore

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var dummyJson = require('dummy-json');
+var minify = require('html-minifier').minify;
 
 // Build some dummy JSON
 var jsontemplate = fs.readFileSync('dummyjson.hbs', {encoding: 'utf8'});
@@ -7,8 +8,11 @@ var dummyData = JSON.parse(dummyJson.parse(jsontemplate));
 
 var userData = '';
 
+// Build up the userData HTML
 dummyData.users
+    // only return true if user.optedin is true, so only pass through users who have opted in
     .filter( user => user.optedin )
+    // Add an <li> for each user
     .forEach( user => {
         userData += `
         <li class="user">
@@ -30,4 +34,20 @@ fs.writeFile("populated.html", replaced, function(err) {
     }
 
     console.log("The template was populated");
+});
+
+var minified = minify(replaced, {
+    collapseWhitespace: true,
+    collapseInlineTagWhitespace: true,
+    removeComments: true,
+    removeScriptTypeAttributes: true,
+    removeStyleLinkTypeAttributes: true
+});
+
+fs.writeFile("populated.min.html", minified, function(err) {
+    if(err) {
+        return console.log(err);
+    }
+
+    console.log("The minified file was created");
 });
